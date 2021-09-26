320a450153e0d5c27aa3c39778284b7330b09f80
MSang <msang77335@gmail.com>
2021-09-26 16:32:11 +0700

    update book route

--- src/routes/book.route.js
+++ src/routes/book.route.js
@@ -7,46 +7,67 @@ const router = express.Router();
 
 router.get("/", auth, async function (req, res) {
    const data = await bookModel.find({});
-   res.json({ data: data });
+   if (!data) {
+      res.status(200).json({ fetch: false, data: data });
+   }
+   res.status(200).json({ fetch: true, data: data });
 });
 
 router.get("/:id", auth, async function (req, res) {
    const id = req.params.id || "0";
    const data = await bookModel.findOne({ id: id });
-   res.json({ data: data });
+   if (!data) {
+      res.status(200).json({
+         fetch: false,
+         data: data,
+         message: "Book id invalid!",
+      });
+   }
+   res.status(200).json({ fetch: true, data: data });
 });
 
 router.get("/category/:category", auth, async function (req, res) {
    const categoryId = req.params.category || "0";
    const data = await bookModel.find({ category: categoryId });
-   res.json({ data: data });
+   if (!data) {
+      res.status(200).json({
+         fetch: false,
+         data: data,
+         message: "Category id invalid!",
+      });
+   }
+   res.status(200).json({ fetch: true, data: data });
 });
 
-router.post("/", validate(bookModel), async function (req, res) {
+router.post("/", auth, validate(bookModel), async function (req, res) {
    const book = req.body;
-   const result = await new bookModel(book).save();
-   res.status(201).json({ book: result, added: true });
+   await new bookModel(book).save();
+   res.status(201).json({ add: true, message: "Add book success!!!" });
 });
 
-router.patch("/:id", validate(bookModel), async function (req, res) {
+router.patch("/:id", auth, validate(bookModel), async function (req, res) {
    const id = req.params.id || "0";
    const newBook = req.body;
    const book = await bookModel.findOne({ id: id });
    if (!book) {
-      return res.status(201).json({ book: book, updated: false });
+      return res
+         .status(201)
+         .json({ update: false, message: "Book id invalid!" });
    }
-   const result = await bookModel.updateOne({ id: id }, newBook);
-   res.status(201).json({ book: result, updated: true });
+   await bookModel.updateOne({ id: id }, newBook);
+   res.status(201).json({ update: true, message: "Update book success!!!" });
 });
 
-router.delete("/:id", async function (req, res) {
+router.delete("/:id", auth, async function (req, res) {
    const id = req.params.id || "0";
    const book = await bookModel.findOne({ id: id });
    if (!book) {
-      return res.status(201).json({ book: book, deleted: false });
+      return res
+         .status(201)
+         .json({ delete: false, message: "Book id invalid!" });
    }
-   const result = await book.remove();
-   res.status(201).json({ book: result, deleted: true });
+   await book.remove();
+   res.status(201).json({ delete: true, message: "Delete book success!!!" });
 });
 
 module.exports = router;
