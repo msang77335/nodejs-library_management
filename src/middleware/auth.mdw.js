85791f65e5ff3b8b39b09e5a54b289e417580d7a
MSang <msang77335@gmail.com>
2021-09-26 16:25:19 +0700

    update category route

--- src/middleware/auth.mdw.js
+++ src/middleware/auth.mdw.js
@@ -4,6 +4,7 @@ module.exports = function auth(req, res, next) {
    const accessToken = req.headers["x-access-token"];
    if (!accessToken) {
       return res.status(401).json({
+         authen: false,
          message: "Access token not found!",
       });
    } else {
@@ -13,6 +14,7 @@ module.exports = function auth(req, res, next) {
          next();
       } catch (err) {
          return res.status(401).json({
+            authen: false,
             message: "Invalid access token!",
          });
       }
--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -7,40 +7,60 @@ const router = express.Router();
 
 router.get("/", auth, async function (req, res) {
    const data = await categoryModel.find({});
-   res.json({ data: data });
+   if (!data) {
+      res.status(200).json({ fetch: false, data: data });
+   }
+   res.status(200).json({ fetch: true, data: data });
 });
 
 router.get("/:id", auth, async function (req, res) {
    const id = req.params.id || "0";
    const data = await categoryModel.findOne({ id: id });
-   res.json({ data: data });
+   if (!data) {
+      res.status(200).json({
+         fetch: false,
+         data: data,
+         message: "Category id invalid!",
+      });
+   }
+   res.status(200).json({ fetch: true, data: data });
 });
 
-router.post("/", validate(categoryModel), async function (req, res) {
+router.post("/", auth, validate(categoryModel), async function (req, res) {
    const category = req.body;
-   const result = await new categoryModel(category).save();
-   res.status(201).json({ category: result, added: true });
+   await new categoryModel(category).save();
+   res.status(201).json({ add: true, message: "Add category success!!!" });
 });
 
-router.patch("/:id", validate(categoryModel), async function (req, res) {
+router.patch("/:id", auth, validate(categoryModel), async function (req, res) {
    const id = req.params.id || "0";
    const newCategory = req.body;
    const category = await categoryModel.findOne({ id: id });
    if (!category) {
-      return res.status(201).json({ category: category, updated: false });
+      return res
+         .status(201)
+         .json({ update: false, message: "Category id invalid!" });
    }
-   const result = await categoryModel.updateOne({ id: id }, newCategory);
-   res.status(201).json({ category: result, updated: true });
+   await categoryModel.updateOne({ id: id }, newCategory);
+   res.status(201).json({
+      updated: true,
+      message: "Update category success!!!",
+   });
 });
 
-router.delete("/:id", async function (req, res) {
+router.delete("/:id", auth, async function (req, res) {
    const id = req.params.id || "0";
    const category = await categoryModel.findOne({ id: id });
    if (!category) {
-      return res.status(201).json({ category: category, deleted: false });
+      return res
+         .status(201)
+         .json({ delete: false, message: "Category id invalid!" });
    }
-   const result = await category.remove();
-   res.status(201).json({ category: result, deleted: true });
+   await category.remove();
+   res.status(201).json({
+      delete: true,
+      message: "Delete category success!!!",
+   });
 });
 
 module.exports = router;
